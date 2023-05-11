17016200a3cef5f8562fe540d82b759629a2bb76
Thyago Raphael <thyagoraphael1987@gmail.com>
2023-05-10 23:51:47 -0300

    feat: Add class DigitalWalletPayment

--- Mentoria/Mentoria/Feature/DigitalWalletPayment.swift
+++ Mentoria/Mentoria/Feature/DigitalWalletPayment.swift
@@ -6,3 +6,41 @@
 //
 
 import Foundation
+
+final class DigitalWalletPayment: TransactionProtocol {
+    let digitalWalletNumber: Int
+    let amount: Double
+    let account: Account
+
+    init(
+        digitalWalletNumber: Int,
+        amount: Double,
+        account: Account
+    ) {
+        self.digitalWalletNumber = digitalWalletNumber
+        self.amount = amount
+        self.account = account
+    }
+
+    func validate() throws {
+        // Verifica se existe número da conta digital
+        let convertDigitalWalletNumberToString = String(digitalWalletNumber)
+        guard !convertDigitalWalletNumberToString.isEmpty else {
+            throw ValidationError.invalidCardNumber
+        }
+
+        // Verifica se o saldo é maior que 0
+        guard amount > 0 else {
+            throw ValidationError.invalidAmount
+        }
+        
+        // Verifica se a conta tem saldo
+        guard account.balance > 0 else {
+            throw ValidationError.invalidBalance
+        }
+    }
+
+    func processTransaction() throws {
+        try validate()
+    }
+}
