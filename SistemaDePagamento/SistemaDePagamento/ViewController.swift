631a0a8e501f4d934a60c698b053b52d2d2bfb85
Kleiton Mendes <kleiton_mendes@hotmail.com>
2023-05-11 13:41:23 -0300

    Iniciando a Sprint 2, implementando o SOLID

--- SistemaDePagamento/SistemaDePagamento/ViewController.swift
+++ SistemaDePagamento/SistemaDePagamento/ViewController.swift
@@ -11,149 +11,217 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
     }
 }
 
-    class Transaction {
-        let valor: Int
-        
-        init(valor: Int) {
-            self.valor = valor
-        }
-        
-        func processTransaction() {
-            print("Processo de transação")
-        }
-        
-        func validate() { }
-    }
+enum TransactionError: Error {
+    case failCreditCardPayment
+    case failDebitCardPayment
+    case failDigitalWalletPayment
+    case accountNotFound
+}
 
-    class BankTransfer: Transaction {
-        let contaOrigem: Int
-        let contaDestino: Int
-        
-        init(contaOrigem: Int, contaDestino: Int, valor: Int) {
-            self.contaOrigem  = contaOrigem
-            self.contaDestino = contaDestino
-            super.init(valor: valor)
-        }
-        
-        override func processTransaction() {
-            super.processTransaction()
-            print("Conta e destino estão corretos")
-        }
-        
-        override func validate() { }
-    }
+enum PasswordError: Error {
+    case incorrectPassword, passwordNotMatch
+}
 
-    class CreditCardPayment: Transaction {
-        let numeroCartao: String
-        let dataVencimento: Int
-        let codigoSeguranca: Int
-        let nomeTitularCartao: String
-        
-        init(numeroCartao: String, dataVencimento: Int, codigoSeguranca: Int, nomeTitularCartao: String, valor: Int ) {
-            self.numeroCartao = numeroCartao
-            self.dataVencimento = dataVencimento
-            self.codigoSeguranca = codigoSeguranca
-            self.nomeTitularCartao = nomeTitularCartao
-            super.init(valor: valor)
+protocol Transaction {
+    let valor: Int { get }
+    func processTransaction()
+    func validate()
+}
+
+class BankTransfer: Transaction {
+    
+    let valor: Int
+    
+#if DEBUG && targetEnvironment(simulator)
+    func processTransaction() {
+        print("Conta e destino estão corretos")
+    }
+    
+    func validate() {
+        if contaOrigem != nil &&
+            contaDestino != nil {
+            print("Ir para a tela de confirmação de transação")
         }
-        
-        override func processTransaction() {
-            super.processTransaction()
-            print("Pagamento com Cartão de Crédito efetuado com Sucesso")
+    }
+#endif
+    
+    let contaOrigem: Int
+    let contaDestino: Int
+    
+    init(contaOrigem: Int, contaDestino: Int, valor: Int) {
+        self.contaOrigem  = contaOrigem
+        self.contaDestino = contaDestino
+    }
+#if DEBUG && targetEnvironment(simulator)
+    func processTransaction() {
+        print("Conta e destino estão corretos")
+    }
+    
+    func validate() {
+        if contaOrigem != nil &&
+            contaDestino != nil {
+            print("Ir para a tela de confirmação de transação")
         }
-        #if DEBUG && targetEnvironment(simulator)
-        override func validate() {
-        let number: Int = 6500400300322110
-        let cvc: Int = 356
+    }
+#endif
+    
+    func failProcessTransaction() throws -> String {
+        if contaOrigem == nil &&
+            contaDestino == nil {
+            throw TransactionError.accountNotFound
+        }
+    }
+    
+    do {
+        try TransactionError.accountNotFound
+    } catch {
+        print(error)
+    }
+}
+
+class CreditCardPayment: Transaction {
+    
+    var valor: Int
+    
+    let numeroCartao: String
+    let dataVencimento: Date
+    let codigoSeguranca: Int
+    let nomeTitularCartao: String
+    
+    init(numeroCartao: String, dataVencimento: Int, codigoSeguranca: Int, nomeTitularCartao: String, valor: Int ) {
+        self.numeroCartao = numeroCartao
+        self.dataVencimento = dataVencimento
+        self.codigoSeguranca = codigoSeguranca
+        self.nomeTitularCartao = nomeTitularCartao
+    }
+    
+    func processTransaction() {
+        print("Pagamento com Cartão de Crédito efetuado com Sucesso")
+    }
+#if DEBUG && targetEnvironment(simulator)
+    func validate() {
+        let number: String = "6500400300322110"
+        let cvc: String = "356"
         let name: String = "Lebron James"
         let nameCard: String = "Lebron James"
         let dateCard: String = "12/28"
-            
-            if number == number.count && cvc == cvc.count && name == nameCard && dateCard == dateCard {
-                print("Informações conferem com o Cartão Cadastrado")
-            } else {
-                print("Atenção!! As informações estão incorretas")
-            }
+        
+        if number.count == number.count && cvc.count == cvc.count && name == nameCard && dateCard == dateCard {
+            print("Informações conferem com o Cartão Cadastrado")
+        } else {
+            TransactionError.failCreditCardPayment
         }
-        #endif
     }
+#endif
+    
+    func validadeCreditCardPayment() throws -> String {
+            throw TransactionError.failCreditCardPayment
+        }
+ 
+    do {
+        TransactionError.failCreditCardPayment
+    } catch {
+        print(error)
+    }
+    
+}
+    
+class DebitCardPayment: Transaction{
+    
+    let valor: Int
+    
+    let numeroCartao: String
+    let dataVencimento: Int
+    let codigoSeguranca: Int
+    let nomeTitularCartao: String
+    
+    init(numeroCartao: String, dataVencimento: Int, codigoSeguranca: Int, nomeTitularCartao: String, valor: Int ) {
+        self.numeroCartao = numeroCartao
+        self.dataVencimento = dataVencimento
+        self.codigoSeguranca = codigoSeguranca
+        self.nomeTitularCartao = nomeTitularCartao
+    }
+    
+     func processTransaction() {
+        print("Pagamento com Cartão de Débito efetuado com Sucesso")
+    }
+    
+#if DEBUG && targetEnvironment(simulator)
+     func validate() {
+        let password: String = "04567346"
+        if password == password.count {
+            print("Senha valida. Compra Efetuada com Sucesso")
+        } else {
+            PasswordError.incorrectPassword
+        }
+    }
+#endif
+}
 
-    class DebitCardPayment: Transaction{
-        let numeroCartao: String
-        let dataVencimento: Int
-        let codigoSeguranca: Int
-        let nomeTitularCartao: String
-        
-        init(numeroCartao: String, dataVencimento: Int, codigoSeguranca: Int, nomeTitularCartao: String, valor: Int ) {
-            self.numeroCartao = numeroCartao
-            self.dataVencimento = dataVencimento
-            self.codigoSeguranca = codigoSeguranca
-            self.nomeTitularCartao = nomeTitularCartao
-            super.init(valor: valor)
+class DigitalWalletPayment: Transaction {
+    
+    let valor: Int
+    
+    func validate() {
+        if let password.isEmpty {
+            print("Pagamento Efetuado com Sucesso")
         }
-        
-        override func processTransaction() {
-            super.processTransaction()
-            print("Pagamento com Cartão de Débito efetuado com Sucesso")
-        }
-    #if DEBUG && targetEnvironment(simulator)
-        override func validate() {
-            let password: Int = 04567346
-            if password == password.count {
-                print("Senha valida. Compra Efetuada com Sucesso")
-            } else {
-                print("Senha incorreta. Digitar a senha correta do cartão")
-            }
-        }
-        #endif
     }
+    
+    var numeroCarteiraDigital: Int
+    
+    init(numeroCarteiraDigital: Int, valor: Int) {
+        self.numeroCarteiraDigital = numeroCarteiraDigital
+    }
+    
+     func processTransaction() {
+        print("Pagamento com Cartão de Débito efetuado com Sucesso")
+    }
+    
+    func failtProcessTransaction() throws -> String {
+        TransactionError.failDebitCardPayment
+    }
+    
+    do {
+        try? TransactionError.failDebitCardPayment
+    } catch {
+        print(error)
+    }
+    
+}
 
-    class DigitalWalletPayment: Transaction {
-        var numeroCarteiraDigital: Int
-        
-        init(numeroCarteiraDigital: Int, valor: Int) {
-            self.numeroCarteiraDigital = numeroCarteiraDigital
-            super.init(valor: valor)
-        }
-        
-        override func processTransaction() {
-            super.processTransaction()
-            print("Pagamento com Cartão de Débito efetuado com Sucesso")
+class TransactionProcessor {
+    func processTransactions(transaction:[Transaction] ) {
+        for transactions in transaction {
+            transaction.count
         }
     }
+}
 
-    class TransactionProcessor {
-        func processTransactions(transaction:[Transaction] ) {
-            for transactions in transaction {
-                transaction.count
-            }
-        }
+class CreateAccount {
+    let fullName: String
+    let cpf: Int
+    let address: String
+    let dateBirth: Date
+    let state: String
+    let city: String
+    
+    init(fullName: String, cpf: Int, address: String, dateBirth: Date, state: String, city: String) {
+        self.fullName = fullName
+        self.cpf = cpf
+        self.address = address
+        self.dateBirth = dateBirth
+        self.city = city
     }
-
-    class CreateAccount {
-        let fullName: String
-        let cpf: Int
-        let address: String
-        let dateBirth: Date
-        let state: String
-        let city: String
-        
-        init(fullName: String, cpf: Int, address: String, dateBirth: Date, state: String, city: String) {
-            self.fullName = fullName
-            self.cpf = cpf
-            self.address = address
-            self.dateBirth = dateBirth
-            self.city = city
-        }
-        
-        func contaOrigem() { }
-        
-        func contaDestino() { }
-    }
+    
+    func contaOrigem() { }
+    
+    func contaDestino() { }
+}
 
 //    @discardableResult
 
