98b1aa1327a0d419243b0539fbd2db13918ef228
Thyago Raphael <thyagoraphael1987@gmail.com>
2023-05-10 23:52:20 -0300

    feat: Add class TransactionProcessor

--- Mentoria/Mentoria/Feature/TransactionProcessor.swift
+++ Mentoria/Mentoria/Feature/TransactionProcessor.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+class TransactionProcessor {
+    func processTransactions(transactions: [TransactionProtocol]) {
+        transactions.forEach {
+            do {
+                try $0.processTransaction()
+                print("Transação realizada com sucesso")
+            } catch {
+                guard let error = error as? ValidationError else { return }
+                print(error.messageResult)
+            }
+        }
+    }
+}
